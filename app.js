ab028d6506aa0fd3b40cc2aecd3fc04c9ba4316b
DillEmma <emmabauta@gmail.com>
2021-01-27 17:40:05 -0500

    started inquirer prompt and data tables

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-const inquirer = require ("inquirer");
-const logo = require ("asciiart-logo"); 
-const db = require ('./db'); 
-require("console.table"); 
+const inquirer = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require('./db');
+require("console.table");
 
 function init() {
-    const logoText = logo ({ name: "Em's Employee Manager"}).render(); 
+    const logoText = logo({ name: "Em's Employee Manager" }).render();
     console.log(logoText);
+    loadMainMenu();
+}
+
+function loadMainMenu() {
+    inquirer.prompt([
+        {
+            name: 'choice',
+            type: 'list',
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View all employees",
+                    value: "VIEW_EMPLOYEES"
+                }
+
+            ]
+        }
+    ]).then(function (choices) {
+        console.log(choices);
+        handleChoices(choices);
+    })
+}
+
+function handleChoices(choices) {
+    switch (choices.choices) {
+        case "VIEW_EMPLOYEES":
+            return viewEmployees()
+    }
 } 
+
+
+
+
+
+
+
+
+
+
+
 init()
--- db/connection.js
+++ db/connection.js
@@ -7,10 +7,10 @@ const connection = mysql.createConnection({
 });
  
 connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
-  });
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
