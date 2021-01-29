bcff331855d2cc1ca28ba0c57730f77024e2633b
DillEmma <emmabauta@gmail.com>
2021-01-28 20:05:03 -0500

    added switch statement for first choice, updated seeds, and schema.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const inquirer = require("inquirer");
 const logo = require("asciiart-logo");
-const db = require('./db');
 require("console.table");
+const mysql = require('mysql'); 
 
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'rootroot', 
+  database : 'employees'
+}); 
+
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  
+  console.log('connected as id ' + connection.threadId);
+});
 function init() {
     const logoText = logo({ name: "Em's Employee Manager" }).render();
     console.log(logoText);
@@ -17,10 +33,31 @@ function loadMainMenu() {
             message: "What would you like to do?",
             choices: [
                 {
-                    name: "View all employees",
-                    value: "VIEW_EMPLOYEES"
-                }
+                    name: "Add a department",
+                    value: "ADD_DEPARTMENT", 
+                }, 
 
+                {
+                    name: "Add an employee", 
+                    value: "ADD_EMPLOYEE",
+                }, 
+
+                {   name: "Add a role", 
+                    value: "ADD_ROLE", 
+                },
+
+                {   name: "View by department", 
+                    value: "VIEW_DEPARTMENT", 
+                },
+                
+                {   name: "View by role", 
+                    value: "VIEW_ROLE", 
+                },
+
+                {
+                    name: "View employee", 
+                    value: "VIEW_EMPLOYEE"
+                }
             ]
         }
     ]).then(function (choices) {
@@ -32,9 +69,20 @@ function loadMainMenu() {
 function handleChoices(choices) {
     switch (choices.choices) {
         case "VIEW_EMPLOYEES":
-            return viewEmployees()
+            return viewEmployees()  
+        case "VIEW_DEPARTMENT": 
+            return viewDepartment()
+        case "VIEW_ROLE": 
+            return viewRole() 
+        case "ADD_EMPLOYEE": 
+            return addEmployee() 
+        case "ADD_DEPARTMENT": 
+            return addDepartment() 
+        case "ADD_ROLE": 
+            return addRole()
     }
-} 
+}  
+
 
 
 
