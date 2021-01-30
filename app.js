07a411867f14610b1046ded49734c46df7725d21
DillEmma <emmabauta@gmail.com>
2021-01-29 22:44:50 -0500

    added add functionality

--- app.js
+++ app.js
@@ -93,25 +93,108 @@ function viewEmployees() {
     var query = "SELECT * FROM employee";
     connection.query(query, function (err, res) {
         if (err) throw err;
-        console.table(res); 
+        console.table(res);
     })
 }
 
-function viewDepartment(){
-    var query = "SELECT * FROM department"; 
-    connection.query(query, function (err, res){
-        if (err) throw err; 
+function viewDepartment() {
+    var query = "SELECT * FROM department";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
         console.table(res);
     })
-} 
+}
 
-function viewRole(){
-    var query = "SELECT * FROM role"; 
-    connection.query(query, function(err, res){
-        if (err) throw err; 
+function viewRole() {
+    var query = "SELECT * FROM role";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
         console.table(res);
     })
-} 
+}
+
+function addDepartment() {
+    inquirer.prompt({
+        name: "department_name",
+        type: "input",
+        message: "What would you like to name the department?"
+    })
+        .then(function (departmentName) {
+            connection.query("INSERT INTO department SET ?", {
+                name: departmentName.department_name
+            },
+                function (err) {
+                    if (err) throw err;
+                    console.log("A new department has been created.");
+                }
+            );
+        });
+
+}
+
+function addEmployee() {
+    inquirer.prompt([
+    {
+        name: "firstName",
+        type: "input",
+        message: "What is the new employee's first name?"
+    },
+    {
+            name: "lastName",
+            type: "input",
+            message: "What is the new employee's last name?" 
+    },
+    {   
+        name: "ID", 
+        type: "input",
+        message: "Input new employee's role id."    
+    }])
+        .then(function (newEmployee) {
+            console.log(newEmployee);
+            connection.query("INSERT INTO employee SET ?", {
+                first_name: newEmployee.firstName,
+                last_name: newEmployee.lastName,
+                role_id: newEmployee.ID
+            },
+                function (err) {
+                    if (err) throw err;
+                    console.log("A new employee has been added.");
+                }
+            );
+        });
+}
+
+function addRole() {
+    inquirer.prompt([
+    {
+        name: "new_role",
+        type: "input",
+        message: "What would you like to call the new role?"
+    },
+    {
+            name: "salary",
+            type: "input",
+            message: "What is the annual salary for this role?" 
+    },
+    {   
+        name: "ID", 
+        type: "input",
+        message: "Input new id for role."    
+    }])
+        .then(function (newRole) {
+            console.log(newRole);
+            connection.query("INSERT INTO role SET ?", {
+                title: newRole.new_role,
+                salary: newRole.salary,
+                department_id: newRole.ID
+            },
+                function (err) {
+                    if (err) throw err;
+                    console.log("A new role has been added.");
+                }
+            );
+        });
+}
 
 
 
